5b14d5df295c08e1449c325d2e0e28a04eb85124
David Sartori <david.sartori@nab.com.au>
2017-03-07 15:19:18 +1100

    Updates

--- routes.js
+++ routes.js
@@ -1,28 +1,55 @@
 const model = require('od-train-data');
+const zooplaService = require('od-train-service');
 
 var getAllProperties = (req, res) => {
-    new model({}).findAll((err, data) => {
-        res.send(data);
-    });
+    model.find({})
+        .then((properties) => res.json(properties))
+        .catch((err) => res.status(500).send(err.message));
 };
 
-var getAllProperty = (req, res) => {
-    new model({listingId: `${req.params.id}`}).findByListingId((err, data) => {
-        res.send(data);
-    });
+var getProperty = (req, res) => {
+    model.findOne({_id: req.params.id})
+        .then((property) => {
+            if (property) {
+                res.json(property)
+            } else {
+                res.send('No property found');
+            }
+        })
+        .catch((err) => res.status(500).send(err.message));
 };
 
 var createProperty = (req, res) => {
+    model.create(req.body)
+        .then((id) => res.send(id))
+        .catch((err) => res.status(500).send(err.message));
+};
 
-    new model(req.body).save((err, data) => {
-        console.log(err);
-        res.send("Saved " + req.body.listingId);
+var updateProperty = (req, res) => {
+    model.updateOne({_id: req.params.id}, req.body)
+        .then((updatedProperty) => res.send(updatedProperty))
+        .catch((err) => res.status(500).send(err.message));
+};
+
+var deleteProperty = (req, res) => {
+    model.remove({_id: req.params.id})
+        .then((updatedProperty) => res.send(updatedProperty))
+        .catch((err) => res.status(500).send(err.message));
+};
+
+var zooplaImport = (req, res) => {
+    zooplaService.getAllPropertyListings(() => {
+
     });
+    res.json({message: "Zoopla data imported"});
 };
 
 module.exports = {
     getAllProperties,
-    getAllProperty,
-    createProperty
+    getProperty,
+    createProperty,
+    updateProperty,
+    deleteProperty,
+    zooplaImport
 }
 
--- server.js
+++ server.js
@@ -9,15 +9,18 @@ app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/properties',     routes.getAllProperties);
-app.get('/properties/:id', routes.getAllProperty);
-app.post('/properties',    routes.createProperty);
+app.get('/properties', routes.getAllProperties);
+app.get('/properties/:id', routes.getProperty);
+app.post('/properties', routes.createProperty);
+app.put('/properties/:id', routes.updateProperty);
+app.delete('/properties/:id', routes.deleteProperty);
+app.post('/properties/zoopla/import', routes.zooplaImport);
 
 app.listen(3000);
 
 // app.get('/about', (req, res) => {
-//     res.render('about.hbs', {
-//        pageTitle: 'Title hhh',
-//        currentYear: new Date().getFullYear()
+// res.render('about.hbs', {
+//    pageTitle: 'Title hhh',
+//    currentYear: new Date().getFullYear()
 //    });
 // });
